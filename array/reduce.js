0676e05d04dfe2f3173e07a3a96040f9ac9b345e
Nícolas Gabriel <nickygamer023@gmail.com>
2022-12-16 08:15:48 -0300

    Fixes edge case of null as initial value

--- array/reduce.js
+++ array/reduce.js
@@ -1,6 +1,6 @@
 Array.prototype.reduce = function (callback, initialValue) {
     if(typeof callback !== 'function') throw Error(`TypeError: ${callback} is not a function`)
-    let accumulator = initialValue || this[0]
+    let accumulator = initialValue === undefined ? this[0] : initialValue;
 
     for (let index = initialValue ? 0 : 1; index < this.length; index++) {
         if (this[index] === undefined) continue
