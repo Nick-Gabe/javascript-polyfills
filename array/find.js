043f8c90966a33e3133d8421b23484324b89c72b
Nick Gabe <nicolasgabrielctt@gmail.com>
2023-09-30 14:59:44 -0300

    converts error to type error

--- array/find.js
+++ array/find.js
@@ -1,5 +1,5 @@
 Array.prototype.find = function (callback, thisArg = this) {
-    if(typeof callback !== 'function') throw Error(`TypeError: ${callback} is not a function`)
+    if(typeof callback !== 'function') throw TypeError(`${callback} is not a function`)
     for (let index = 0; index < thisArg.length; index++) {
         if (callback(thisArg[index], index, thisArg)) return thisArg[index]
     }
